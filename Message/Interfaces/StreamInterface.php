27b161335a6006cf2cc850b2888bfcee28e3d3f0
Tobias <cchawps@gmail.com>
2017-11-24 16:55:16 +0100

    Added all functions to StreamInterface

--- Message/Interfaces/StreamInterface.php
+++ Message/Interfaces/StreamInterface.php
@@ -3,5 +3,19 @@ namespace Blog\Message\Interfaces;
 
 interface StreamInterface
 {
-    
+    public function __toString(): string;
+    public function close(): void;
+    public function detach();
+    public function getSize(): ?int;
+    public function tell(): int;
+    public function eof(): bool;
+    public function isSeekable(): bool;
+    public function seek(int $offset, int $whence = SEEK_SET): void;
+    public function rewind(): void;
+    public function isWritable(): bool;
+    public function write(string $string): int;
+    public function isReadable(): bool;
+    public function read(int $length): string;
+    public function getContents(): string;
+    public function getMetadata(?string $key);
 }
